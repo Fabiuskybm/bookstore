25c6c9f823a635bbb00818717d10fb763ceab4f3
fabiuskybm <fabio.benitezmolina@gmail.com>
2025-12-01 12:41:24 +0000

    feat: add base layout structure

--- public/index.php
+++ public/index.php
@@ -1,17 +1,38 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+declare(strict_types=1);
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookstore</title>
-</head>
+// ==============================
+// |  Front controller (index)  |
+// ==============================
 
-<body>
-    <h1>Bookstore</h1>
-    <p>Proyecto conjunto 1ªEvaluación de asignaturas DSW - DEW - DOR (2ºDAW)</p>
-    
-    <script src="assets/js/main.js"></script>
-</body>
 
-</html>
+
+// ------------------
+//   CARGAR MÓDULOS  
+// ------------------
+
+// Helpers de presentación (función e())
+require_once __DIR__ . '/../src/Shared/html.php';
+
+
+
+// -----------------------
+//   Procesar vistas GET  
+// -----------------------
+
+// 1. Determinar vista solicitada (home por defecto)
+$view = $_GET['view'] ?? 'home';
+
+// 2. Resolver archivo de vista
+switch ($view) {
+
+    case 'home':
+    default:
+        $viewFile = __DIR__ . '/../src/Home/views/home.php';
+        $pageTitle = 'Bookstore | Home';
+        break;
+
+}
+
+// 3. Cargar layout común
+require __DIR__ . '/../src/Shared/templates/layout.php';
---
+++ src/Shared/templates/layout.php
@@ -0,0 +1,36 @@
+<?php 
+declare(strict_types=1);
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= e($pageTitle ?? 'Bookstore') ?></title>
+    <!-- <link rel="stylesheet" href="assets/css/main.css"> -->
+</head>
+
+<body class="page">
+
+    <header class="page__header header"></header>
+    
+    <main class="page__main">
+        <?php 
+            if (isset($viewFile) && file_exists($viewFile)) {
+                require $viewFile;
+            } else {
+                echo '<p class="page__error">Vista no encontrada.</p>';
+            }
+        ?>
+    </main>
+
+    <footer class="page__footer footer"></footer>
+    
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
