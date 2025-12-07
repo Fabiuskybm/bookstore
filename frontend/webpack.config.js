f6490fe34f88caeffc06fa32717dff5a3b716ce1
Fabio Benítez <145270161+Fabiuskybm@users.noreply.github.com>
2025-12-07 01:21:51 +0000

    feat: unify and apply base style system

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -13,6 +13,10 @@ export default {
             {
                 test: /\.scss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
